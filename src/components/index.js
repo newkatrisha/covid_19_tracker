b48fd39dc107bd793f2d29bd0d28af8298e540b7
Ekaterina Khaykin <newkatrisha@gmail.com>
2020-07-12 14:54:55 +0300

    Initial set up of corona project

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// this file is going to take all of the components(Cards, Chart...etc) and it's going
+// to export it from the components folder
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
